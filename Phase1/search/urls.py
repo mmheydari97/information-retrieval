4c576d82d9aef22a5fd0ec821fdb7af658570774
mmheydari97 <GDE in 2012>
2019-10-29 01:00:26 +0330

    connect search

--- Phase1/search/urls.py
+++ Phase1/search/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from search import views
 
 
 urlpatterns = [
-    path('', views.SearchView.as_view(), name="search"),
-    path("q", views.NewsListView.as_view(), name="news_list"),
-    path(r'news/(?P<pk>\d+)', views.NewsDetailView.as_view(), name='news_detail'),
+    # path('', views.SearchView.as_view(), name="search"),
+    path('', views.search, name="search"),
+    path("news", views.NewsListView.as_view(), name="news_list"),
+    re_path(r'news/(?P<pk>\d+)', views.NewsDetailView.as_view(), name='news_detail'),
 ]
--- Phase1/search/views.py
+++ Phase1/search/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView, DetailView
 from .models import News
 
 
 # Create your views here.
-class SearchView(TemplateView):
-    template_name = 'index.html'
+# class SearchView(TemplateView):
+#     template_name = 'index.html'
+query = None
+
+
+def search(request):
+    if not request.POST or request.POST["query"] == "":
+        return render(request, "index.html")
+
+    else:
+        global query
+        query = request.POST["query"]
+        return redirect("news_list")
 
 
 class NewsListView(ListView):
@@ -13,6 +24,8 @@ class NewsListView(ListView):
 
     def get_queryset(self):
         # TODO: adjust doc_ids based on the query
+        # doc_ids = foo(query)
+
         doc_ids = [1, 3, 5, 7]
 
         return News.objects.filter(doc_id__in=doc_ids).order_by("-publish_date")
